22d188499c3b3b872be96380f5bafb9396156fb6
Dyneteve <sound0020@gmail.com>
2019-12-25 18:42:39 +0000

    sdm660-common: Add NexusLauncherRelease to dexpreopt apps

--- sdm660.mk
+++ sdm660.mk
@@ -374,7 +374,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
 # Preopt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI \
+    NexusLauncherRelease
 
 # Privapp-Permissions
 PRODUCT_COPY_FILES += \
