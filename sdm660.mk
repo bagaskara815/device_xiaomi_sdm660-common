f02eccf67b124d8f6080e91e1e6071930d524a8d
Michael Bestas <mkbestas@lineageos.org>
2020-01-04 20:56:13 +0200

    sdm660-common: Reenable HFP and speaker protection
    * Looks like those libs were opensource
      so we can just compile them
    
    Change-Id: I61d2815346f6177204967a6ec2c05123ce5c1bc6
    
    sdm660: derp

--- sdm660.mk
+++ sdm660.mk
@@ -57,6 +57,7 @@ PRODUCT_PACKAGES += \
     libhdmiedid \
     libhfp \
     libsndmonitor \
+    libspkrprot \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
@@ -127,8 +128,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
+    android.hardware.camera.provider@2.5
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
