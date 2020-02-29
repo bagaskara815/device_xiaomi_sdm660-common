f3be4cb1316c360a11f688e3d694e17e38bec43b
bagaskara815 <bagaskara815@gmail.com>
2020-02-29 23:59:05 +0000

    Set neverallow

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -230,6 +230,7 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
