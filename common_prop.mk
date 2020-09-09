dbf19c1b23ab75da55c85332d5859c84c848c6fc
Akhil MS <Akhilsree001@gmail.com>
2020-09-09 12:10:53 +0000

    Reduce jitter and Make UI more smooth
    Relax DispSync offsets to -8/-12
    
    Relaxes the DispSync offsets so that SurfaceFlinger wakes up at
    -8.6 ms and apps wake up at -12.6 such that SF doesn't get into a bad
    state when device clocks drop.
    
    Bug: 29413700

--- common_prop.mk
+++ common_prop.mk
@@ -318,8 +318,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 
 
 #Storaged
  PRODUCT_PROPERTY_OVERRIDES += \
