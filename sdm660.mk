31f7e414c1056839db758c1040bb031a769d3a10
Danny Lin <danny@kdrag0n.dev>
2020-11-03 22:50:10 -0800

    sdm660-common: overlay: add SimpleDeviceConfig from proton
    Now that we have a custom simple DeviceConfig provisioning service,
    define some Device Personalization Services values to for it to set.
    
    These settings are required for Live Captions and Now Playing to work
    properly without GMS Phenotype provisioning on Pixels.
    
    Change-Id: Ie6d1e1a891d7c1958122101a08b61fc9eb5a4018

--- sdm660.mk
+++ sdm660.mk
@@ -525,6 +525,10 @@ BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 # Shims
 PRODUCT_PACKAGES += \
     camera.sdm660_shim
+    
+# SimpleDeviceConfigOverlay
+PRODUCT_PACKAGES += \
+    SimpleDeviceConfigOverlay
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
