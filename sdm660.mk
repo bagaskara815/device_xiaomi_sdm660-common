25a56165d3be88716309aa91e21e38b3c9bf8b06
Arvind Kumar <karvin@codeaurora.org>
2020-12-03 01:29:43 +0000

    sdm660-common: Enable Health hal 2.1

--- sdm660.mk
+++ sdm660.mk
@@ -271,7 +271,9 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
