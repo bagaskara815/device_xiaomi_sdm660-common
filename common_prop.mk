ba65edad1c3d812a6994e00fbdc612fe7a765057
bagaskara815 <bagaskara815@gmail.com>
2021-01-14 11:49:22 +0000

    sdm660-common: remove whatsapp and camera2 from hal1
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- common_prop.mk
+++ common_prop.mk
@@ -188,8 +188,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # HAL1 apps list
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.hal1.packagelist=com.whatsapp,com.android.camera,com.android.camera2,com.instagram.android \
-    vendor.camera.hal1.packagelist= com.whatsapp,com.android.camera,com.android.camera2,com.instagram.android
+    camera.hal1.packagelist=com.android.camera,com.instagram.android \
+    vendor.camera.hal1.packagelist= com.android.camera,com.instagram.android
 
 # iwlan vowifi corresponding
 PRODUCT_PROPERTY_OVERRIDES += \
