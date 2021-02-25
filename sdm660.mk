ad330d2437ed579b31f00e662e2139883ab4d269
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-02-25 20:31:33 +0530

    sdm660-common: power-libperfmgr: Switch to AIDL Power HAL hardware/google/pixel/power-libperfmgr from android-11.0.0_r25
    SQUASHED:
    Revert all HIDL interface and nuke previous changes
    Import Pixel libperfmgr AIDL Power HAL
    Adapt and rebrand for xiaomi_sdm660
    Remove Google-specific display LPM control
    Remove Google-specific camera and audio hints
    Remove VR hints handling
    Remove audio hints handling
    Remove dumpstate support
    Initialize powerHAL when boot is completed
    Add support for tap-to-wake feature control
    Add sepolicy rules for power-libperfmgr
    Enable power-libperfmgr
    
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>
    
    Conflicts:
    	sepolicy/vendor/hal_power_default.te

--- sdm660.mk
+++ sdm660.mk
@@ -385,15 +385,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 # RRO configuration
 TARGET_USES_RRO := true
 PRODUCT_ENFORCE_RRO_TARGETS := *
-
-# Powerhint
-ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
-    PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/power-libperfmgr/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-else
-    PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-endif
     
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -443,6 +434,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
+
+# Powerhint
+ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
+    PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/power-libperfmgr/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+else
+    PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+endif
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
