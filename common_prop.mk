2730df78ec7725ecdd74a14a80e94c9280c0b2ce
OdSazib <odsazib@gmail.com>
2021-05-17 01:10:07 +0600

    sdm660-common: props: Simplify apex prop  This prop is enabled by default when we inherit https://github.com/OdSazib/device_xiaomi_sdm660-common/blob/11/sdm660.mk#L38

--- common_prop.mk
+++ common_prop.mk
@@ -1,9 +1,3 @@
-# Apex
-ifeq ($(ENABLE_APEX), true)
-PRODUCT_PRODUCT_PROPERTIES += \
-     ro.apex.updatable=true
-endif
-
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.boot-dex2oat-threads=8 \
