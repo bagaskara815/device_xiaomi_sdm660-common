afcc7df94e0698d6e9510f96fdb61e62516527d0
OdSazib <odsazib@gmail.com>
2021-06-15 02:40:18 +0600

    sdm660-common: Switch back to working WFD blobs - LA.UM.9.2.r1-01800-SDMxx0.0 blobs generate glitches like green hue on wifi display - LA.UM.9.6.2.r1-03300-89xx.0 blobs are stable and work on 4.19 too
    This reverts commit b79e69464fcfff0b55956aa89e5182a7c69ca4e7.
    
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -88,12 +88,15 @@ function blob_fixup() {
         
     system_ext/lib64/libdpmframework.so)
         "${PATCHELF}" --add-needed libcutils_shim.so "${2}"
+
+    lib64/libwfdnative.so)
+        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
         ;;
 
     vendor/bin/mlipayd@1.1)
         "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         ;;
-    
+
     vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
         "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
